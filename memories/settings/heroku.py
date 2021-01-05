7b889d0b39a4c871b70c553d2483044ab80214e3
Alexandr Borodavkin <init220@gmail.com>
2021-01-05 09:48:46 +0700

    Change DEBUG = True

--- memories/settings/heroku.py
+++ memories/settings/heroku.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://init220app.herokuapp.com/']
+ALLOWED_HOSTS = ['init220app.herokuapp.com']
 
 # Application definition
 
